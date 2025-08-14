cf5ec6aa9fdd61a79ec2427e7ef0f6a70fea6643
Sanjana-Y-K <sanjanayk7102001@gmail.com>
2025-08-14 16:10:05 +0530

    Create manual.h

---
+++ manual.h
@@ -0,0 +1,13 @@
+#ifndef TELCO_MANUAL_H_
+#define TELCO_MANUAL_H_
+
+#include <iosfwd>  
+#include <string>
+
+namespace TelCoColorCoder {
+
+std::string FormatReferenceManual();
+void PrintReferenceManual(std::ostream& os);
+}  // namespace TelCoColorCoder
+
+#endif // TELCO_MANUAL_H_
